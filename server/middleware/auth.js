982797a1d5cbd60bb39a2b721c1d9aa76e8966da
RemyCodes0 <tuyishimechristophe84@gmail.com>
2025-05-15 00:17:43 +0100

    authentificaation first part

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -7,7 +7,10 @@ const verifyToken= (req,res, next) =>{
     try{
         const decoded = jwt.verify(token, JWT_SECRET);
         req.user = decoded
+        next()
     }catch(err){
-        
+        res.status(403).json({error: 'Invalid tokens'})
     }
 }
+
+module.exports = verifyToken;
