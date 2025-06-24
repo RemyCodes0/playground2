fd7065751813e51f4b046ba3a93c8b7ecb78cdef
RemyCodes0 <tuyishimechristophe84@gmail.com>
2025-06-24 15:24:50 +0100

    added

--- server/index.js
+++ server/index.js
@@ -28,6 +28,9 @@ mongoose.connect(process.env.MONGO_URI)
 app.get('/', (req, res) => {
     res.send("Amea Playground is running")
 })
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => console.log(`server runnning on port ${PORT}`))
